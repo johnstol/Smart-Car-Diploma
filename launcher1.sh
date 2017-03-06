cb9d65bc05cc5da9b142d5cc765e42737bc461f6
johnstol <macjohnsto@gmail.com>
2017-03-06 20:18:13 +0200

    v1

---
+++ launcher1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo NULL > /home/onram/camera.txt
+echo NULL > /home/onram/movement.txt
+echo NULL > /home/onram/sonar.txt
+
+chmod 777 /home/onram/camera.txt
+chmod 777 /home/onram/movement.txt
+chmod 777 /home/onram/sonar.txt
+
+chmod 777 /dev/ttyACM0
+chmod 777 /dev/ttyUSB0
+
+/usr/local/bin/gpio export 17 out
+/usr/local/bin/gpio export 18 out
+
+python /home/pi/scripts/arduino_com.py
