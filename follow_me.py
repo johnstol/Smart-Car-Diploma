45019b48c147c75f3af1789b4003b4a5ea74ca00
johnstol <macjohnsto@gmail.com>
2017-03-06 20:17:45 +0200

    debug start led usage

--- follow_me.py
+++ follow_me.py
@@ -2,20 +2,36 @@ import RPi.GPIO as GPIO ## Import GPIO library
 import serial 
 import time
 
-serout = serial.Serial('/dev/ttyACM0', 19200)	#arduino1 connected via USB
-ser = serial.Serial('/dev/ttyUSB0', 9600)	#arduino2 connected via USB
+#serout = serial.Serial('/dev/ttyACM0', 9600)	#arduino1 connected via USB
+#ser = serial.Serial('/dev/ttyUSB0', 9600)	#arduino2 connected via USB
+
+'''
+#Raspberry Pi 1 model A,B rev2
+red_led=21
+green_led=19
+red_btn=11
+green_btn=7
+'''
+
+
+#Raspberry Pi 3 model B
+red_led=15
+green_led=13
+red_btn=18
+green_btn=16
+
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 
 
-GPIO.setup(19, GPIO.OUT) ## Setup GPIO Pin 19 to OUTPUT
-GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 21 to OUTPUT
+GPIO.setup(green_led, GPIO.OUT) ## Setup GPIO Pin as OUTPUT
+GPIO.setup(red_led, GPIO.OUT) ## Setup GPIO Pin as OUTPUT
 
-GPIO.setup(7, GPIO.IN) ## Setup GPIO Pin 11 to INPUT
-GPIO.setup(11, GPIO.IN) ## Setup GPIO Pin 7 to INPUT
+GPIO.setup(green_btn, GPIO.IN) ## Setup GPIO Pin as INPUT	------16
+GPIO.setup(red_btn, GPIO.IN) ## Setup GPIO Pin as INPUT ----18
 
-GPIO.output(21,True) ##set RED on
-GPIO.output(19,False) ##set GREEN off
+GPIO.output(red_led,True) ##set RED on	----------15
+GPIO.output(green_led,False) ##set GREEN off---------13
 
 red=True;
 green=False;
@@ -26,13 +42,13 @@ def followmefun():
 	global red,green
 	while True:
 		#Stop Follow me 
-		if (GPIO.input(11)):	#stop button (near red LED) pressed
+		if (GPIO.input(red_btn)):	#stop button (near red LED) pressed
 			serout.write("ms")
 			time.sleep(0.01)
 			serout.write("ss")
-			GPIO.output(19,False)	#Light off Green LED
+			GPIO.output(green_led,False)	#Light off Green LED
 			green=False
-			GPIO.output(21,True)	#Light on RED LED
+			GPIO.output(red_led,True)	#Light on RED LED
 			red=True
 			break
 		
@@ -48,7 +64,7 @@ def followmefun():
 				time.sleep(0.01)
 				serout.write("ss")
 				if(red):
-					GPIO.output(21,False)	#Light off RED LED
+					GPIO.output(red_led,False)	#Light off RED LED
 					red=False
 					
 			#move right		
@@ -57,7 +73,7 @@ def followmefun():
 				time.sleep(0.01)
 				serout.write("mr")
 				if(red):
-					GPIO.output(21,False)	#Light off RED LED
+					GPIO.output(red_led,False)	#Light off RED LED
 					red=False
 					
 			#move left		
@@ -66,7 +82,7 @@ def followmefun():
 				time.sleep(0.01)
 				serout.write("ml")
 				if(red):
-					GPIO.output(21,False)	#Light off RED LED
+					GPIO.output(red_led,False)	#Light off RED LED
 					red=False
 			
 			#stop moving
@@ -75,7 +91,7 @@ def followmefun():
 				time.sleep(0.01)
 				serout.write("ss")
 				if(red):
-					GPIO.output(21,False)	#Light off RED LED
+					GPIO.output(red_led,False)	#Light off RED LED
 					red=False
 			
 			#something went wrong
@@ -85,7 +101,7 @@ def followmefun():
 				serout.write("ss")
 				#print(sensors)
 				if not(red):
-					GPIO.output(21,True)	#Light on RED LED
+					GPIO.output(red_led,True)	#Light on RED LED
 					red=True
 					
 			time.sleep(0.05)	
@@ -97,21 +113,31 @@ def followmefun():
 
 while True:
 	#debug only
-	if ((GPIO.input(7)) and (GPIO.input(11))):
-		GPIO.output(21,False)	#Light off RED LED
-		GPIO.output(19,False)	#Light off Green LED
+	if ((GPIO.input(green_btn)) and (GPIO.input(red_btn))):
+		GPIO.output(red_led,False)	#Light off RED LED
+		GPIO.output(green_led,False)	#Light off Green LED
 		#clean up
 		GPIO.cleanup()
 		#terminate
 		break
 
-	if (GPIO.input(7)):	#start button (near green LED) pressed
-		GPIO.output(19,True)	#Light on Green LED
+	if (GPIO.input(green_btn)):	#start button (near green LED) pressed
+		GPIO.output(green_led,True)	#Light on Green LED
 		green=True
-		GPIO.output(21,False)	#Light off RED LED
+		GPIO.output(red_led,False)	#Light off RED LED
 		red=False
 		
+		while True:
+			if (GPIO.input(red_btn)):
+				GPIO.output(green_led,False)
+				green=False
+				GPIO.output(red_led,True)
+				red=True
+				break
+			time.sleep(0.05)
+
+
 		#Follow me started
-		followmefun()
+		#followmefun()
 
 	time.sleep(0.2)		
